0a9c6a5f6bea1b5d14f82eacbe9d5c34f026af4d
Roemer Bakker <contact@roemerbakker.com>
2015-10-04 15:56:36 +0200

    Post limit
    Added a post limit as command line argument. Also changed the way posts
    are printed slightly.

--- lib/rddt/commands/page.rb
+++ lib/rddt/commands/page.rb
@@ -1,17 +1,29 @@
+require 'pp'
+
 module Rddt
   class Command::Page < Rddt::Command
-    desc "front", "Shows front page"
-    def front
+    desc "front LIMIT", "Shows LIMIT number of front page posts"
+    def front(limit=25.to_i)
+
+      # Command line arguments are strings by default
+      limit = limit.to_i
+
+      if limit > 100
+        abort("Limit can't be higher than 100")
+      end
+
       redditUser = Rddt::Reddit::User.new
 
       if client = redditUser.get_authenticated_client
-        frontPagePosts = client.front_page
+        frontPagePosts = client.front_page({:limit => limit})
 
         frontPagePosts.each do |post|
           #ANSI escape sequence to print subreddit with white background
-          print "\e[1;30;47m#{post[:subreddit]}\e[0m"
+          print "\e[1;30;47m#{post[:subreddit].ljust(20)}\e[0m"
+          #ANSI escape sequence to print the upvotes
+          print "\e[1;32;40m#{post[:score]}\e[0m"
           #ANSI escape sequence to print title in bold
-          print "\e[1m#{post[:title]}\e[0m"
+          print "\e[1;30;47m#{post[:title]}\e[0m"
           #End the line
           print "\n"
         end
